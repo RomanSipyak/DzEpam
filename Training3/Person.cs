1685129a0a0d1b6902cd70a5995b51ff0085554e
Roman Sipyak <romansipyak@gmail.com>
2019-08-26 15:04:17 +0300

    add Person

---
+++ Training3/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training3
+{
+    public class  Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public IEnumerable<string> PhoneNumbers { get; set; }
+
+        public override string ToString()
+        {
+            string result = "";
+            foreach (string item in PhoneNumbers)
+            {
+                result += $"Name => {Name},Age => {Age},phone =>{item}\n";
+            }
+            return result;
+        }
+
+    }
+}
