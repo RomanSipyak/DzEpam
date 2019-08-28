148abbcf71ec5b1238a43374533fa023965536e8
Roman Sipyak <romansipyak@gmail.com>
2019-08-28 09:57:45 +0300

    Add tests training1 => Task4

---
+++ Training1Tests/Task4/CircleTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Training1;
+using Training1.Task4;
+
+namespace Training1Tests.Task4
+{
+    [TestClass]
+    public class CircleTests
+    {
+        [TestMethod]
+        public void GetAreaTestTask4()
+        {
+            Assert.AreEqual(36.745, Math.Round(Circle.GetArea(3.42), 3), 0.001);
+            Assert.AreEqual(3.141, Math.Round(Circle.GetArea(1), 3), 0.001);
+            Assert.AreEqual(double.NaN, Circle.GetArea(-2));
+        }
+        [TestMethod]
+        public void GetLengthTestTask4()
+        {
+            Assert.AreEqual(21.488, Math.Round(Circle.GetLength(3.42), 3), 0.001);
+            Assert.AreEqual(6.283, Math.Round(Circle.GetLength(1), 3), 0.001);
+            Assert.AreEqual(double.NaN, Circle.GetArea(0));
+        }
+    }
+}
---
+++ Training1Tests/Task4/RectangleTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Training1;
+using Training1.Task4;
+
+namespace Training1Tests.Task4
+{
+    [TestClass]
+    public class RectangleTests
+    {
+        [TestMethod]
+        public void CalcAreaTestTask4()
+        {
+            Rectangle.LeftTopXCorner = 1.0;
+            Rectangle.LeftTopYCorner = 1.0;
+            Rectangle.RightBottomXCorner = 2.0;
+            Rectangle.RightBottomYCorner = -3.0;
+            Assert.AreEqual(4.0, Math.Round(Rectangle.CalcArea(), 3), 0.001);
+            Rectangle.LeftTopXCorner = 0;
+            Rectangle.LeftTopYCorner = 0;
+            Rectangle.RightBottomXCorner = 10;
+            Rectangle.RightBottomYCorner = -10;
+            Assert.AreEqual(100, Math.Round(Rectangle.CalcArea(), 3), 0.001);
+        }
+        [TestMethod]
+        public void CalcPerimeterTestTask4()
+        {
+            Rectangle.LeftTopXCorner = 1.0;
+            Rectangle.LeftTopYCorner = 1.0;
+            Rectangle.RightBottomXCorner = 2.0;
+            Rectangle.RightBottomYCorner = -3.0;
+            Assert.AreEqual(10.0, Math.Round(Rectangle.CalcPerimeter(), 3), 0.001);
+            Rectangle.LeftTopXCorner = 0;
+            Rectangle.LeftTopYCorner = 0;
+            Rectangle.RightBottomXCorner = 10;
+            Rectangle.RightBottomYCorner = -10;
+            Assert.AreEqual(40, Math.Round(Rectangle.CalcPerimeter(), 3), 0.001);
+            Rectangle.LeftTopXCorner = 0;
+            Rectangle.LeftTopYCorner = 0;
+            Rectangle.RightBottomXCorner = 10;
+            Rectangle.RightBottomYCorner = 10;
+            Assert.AreEqual(40, Math.Round(Rectangle.CalcPerimeter(), 3), 0.001);
+        }
+    }
+}
